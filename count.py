637789a30207c2b3f6cbbde0242e09672fc8fbe1
jackson2213 <37919544+jackson2213@users.noreply.github.com>
2023-02-20 15:15:46 +0800

    create new file
    new file

---
+++ count.py
@@ -0,0 +1,25 @@
+def count_company():
+    articles = pd.read_csv('tweets_3.csv',index_col=['company', 'sensitivity'], low_memory=False)
+    count_company = {}
+    result = []
+    for index, row in tqdm(articles.iterrows(), total = len(articles)):
+        company = index[0]
+        sensitivity = index[1]
+        if company not in count_company.keys():
+             count_company[company] = {'pos':0,'neg':0,'neu':0}
+        else:
+            if sensitivity == 'pos':
+                count_company[company]['pos'] += 1
+            elif sensitivity == 'pos':
+                count_company[company]['neg'] += 1
+            else:
+               count_company[company]['neu'] += 1
+    for key, value in count_company.items():
+        # key value is company
+        pos_ratio = round(value['pos']/(value['pos']+value['neg']+value['neu']),2)
+        neg_ratio = round(value['neg']/(value['pos']+value['neg']+value['neu']),2)
+        neu_ratio = round(value['neu']/(value['pos']+value['neg']+value['neu']),2)
+        result.append([key, pos_ratio, neg_ratio,neu_ratio])
+    df = pd.DataFrame(output, columns = ['company', 'pos_ratio', 'neg_ratio', 'neu_ratio'])  # convert list to dataframe
+    df.to_csv('sensitivity_analyze_by_company.csv')  # save the result to a csv file
+    print('task finished!')
